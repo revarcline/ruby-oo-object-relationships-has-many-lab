596ceafe1816b5d25936422d61ae76b2547a5474
Alex Cline <rev.a.r.cline@gmail.com>
2020-12-18 11:26:05 -0600

    commit tree fd356a7148ab3ab7aa40da49006175f523ba403d

---
+++ lib/author.rb
@@ -0,0 +1,36 @@
+# has many posts
+class Author
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def posts
+    Post.all.select { |post| post.author == self }
+  end
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+  end
+
+  def self.post_count
+    Post.all.length
+  end
+end
