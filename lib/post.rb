c4878479dce02e0b28e80d91003a13e65679ee10
Alex Cline <rev.a.r.cline@gmail.com>
2020-12-18 11:26:11 -0600

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,23 @@
+# post belongs to author
+class Post
+  attr_accessor :author, :title
+
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    author&.name
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+# belongs to artist
+class Song
+  attr_accessor :artist, :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    artist&.name
+  end
+end
